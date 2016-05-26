2ae3ec4e78f1b35b6feca962a4ef4a5e4ca95f58
Peter Purnyn <mpur.nyn@gmail.com>
2016-05-26 14:23:27 -0400

    current version of arduino code

--- Arduino Code/Jefferson_Project/Jefferson_Project.ino
+++ Arduino Code/Jefferson_Project/Jefferson_Project.ino
@@ -130,7 +130,7 @@ void loop() {
   AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
   AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  //Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
   GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
@@ -162,7 +162,7 @@ void loop() {
       //Serial.println(data[1] << 8 | data[2],DEC);
       data[3] = (byte)(AcY >> 8);
       data[4] = (byte)AcY  & 0x00FF;
-      data[5] = (byte)(AcY >> 8);
+      data[5] = (byte)(AcZ >> 8);
       data[6] = (byte)AcZ  & 0x00FF;
       
       data[7] = (byte)(GyX >> 8);
