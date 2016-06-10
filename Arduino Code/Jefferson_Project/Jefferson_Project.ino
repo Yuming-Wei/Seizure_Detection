349181ac66d1d0ff2d3865789377ce9bdad10965
Yuming Wei <yuming.wei@drexel.edu>
2016-06-10 17:29:44 -0400

    delete redundent defines

--- Arduino Code/Jefferson_Project/Jefferson_Project.ino
+++ Arduino Code/Jefferson_Project/Jefferson_Project.ino
@@ -32,28 +32,11 @@ bool TX=1,RX=0,role=0;
 #define MPU6050_GYRO_CONFIG        0x1B   // R/W
 #define MPU6050_ACCEL_CONFIG       0x1C   // R/W
 #define MPU6050_ACCEL_XOUT_H       0x3B   // R  
-
 #define MPU6050_ACCEL_XOUT_L       0x3C   // R    
 
 #define MPU6050_I2C_Addr               0x68   // I2C address of the MPU-6050
 
 #define MPU6050_PWR_MGMT_1             0x6B
-
-#define MPU6050_ACCEL_XOUT_L       0x3C   // R  
-#define MPU6050_ACCEL_YOUT_H       0x3D   // R  
-#define MPU6050_ACCEL_YOUT_L       0x3E   // R  
-#define MPU6050_ACCEL_ZOUT_H       0x3F   // R  
-#define MPU6050_ACCEL_ZOUT_L       0x40   // R  
-#define MPU6050_TEMP_OUT_H         0x41   // R  
-#define MPU6050_TEMP_OUT_L         0x42   // R  
-#define MPU6050_GYRO_XOUT_H        0x43   // R  
-#define MPU6050_GYRO_XOUT_L        0x44   // R  
-#define MPU6050_GYRO_YOUT_H        0x45   // R  
-#define MPU6050_GYRO_YOUT_L        0x46   // R  
-#define MPU6050_GYRO_ZOUT_H        0x47   // R  
-#define MPU6050_GYRO_ZOUT_L        0x48   // R  
-
-#define MPU6050_I2C_Addr               0x68   // I2C address of the MPU-6050
 
 #define MPU6050_ACC_Sen                8192  // Sensitivity for +-4g scale
 #define MPU6050_GYRO_Sen               131  // Sensitivity for +-250 degree/s scale
