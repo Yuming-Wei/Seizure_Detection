4d539ede603305c299155c98770324f4c1165f02
Yuming Wei <yuming.wei@drexel.edu>
2016-06-10 17:32:47 -0400

    delete test serial print

--- Raspberry_Pi/reciever.cpp
+++ Raspberry_Pi/reciever.cpp
@@ -11,11 +11,6 @@ using namespace std;
 // Hardware configuration
 //
 
-<<<<<<< HEAD
-=======
-/****************** Raspberry Pi ***********************/
-
->>>>>>> 47003bb3f46847facbc539dabe0048867f9784da
 // Radio CE Pin, CSN Pin, SPI Speed
 // See http://www.airspayce.com/mikem/bcm2835/group__constants.html#ga63c029bd6500167152db4e57736d0939 and the related enumerations for pin information.
 
@@ -31,15 +26,6 @@ uint8_t recievedata[32];
 //timers for tracking speed of transfer
 unsigned long t0,gnuplotTimer,startTime, stopTime, counter, rxTimer=0;
 
-<<<<<<< HEAD
-=======
-typedef struct{
-	int16_t x;
-	int16_t y;
-	int16_t z;
-} pyStruct;
-
->>>>>>> 47003bb3f46847facbc539dabe0048867f9784da
 int main(int argc, char** argv){
 	bool role_ping_out = 1, role_pong_back = 0;
 	bool role = 0;
@@ -52,22 +38,12 @@ int main(int argc, char** argv){
 	radio.setAutoAck(1);                     // Ensure autoACK is enabled
 	radio.setRetries(2,15);                  // Optionally, increase the delay between retries & # of retries
 	radio.setCRCLength(RF24_CRC_8);          // Use 8-bit CRC for performance
-<<<<<<< HEAD
-	// radio.printDetails();
 	
-=======
-	radio.printDetails();
->>>>>>> 47003bb3f46847facbc539dabe0048867f9784da
 	/********* Role chooser ***********/
 	printf("\n ************ Role Setup ***********\n");
 	string input = "";
 	char myChar = {0};
 	
-	//cout << "Choose a role: Enter 0 for receiver, 1 for transmitter (CTRL+C to exit)\n>";
-<<<<<<< HEAD
-=======
-	//getline(cin,input);
->>>>>>> 47003bb3f46847facbc539dabe0048867f9784da
 	input = "0";
 	if(input.length() == 1) {
 		myChar = input[0];
@@ -91,32 +67,20 @@ int main(int argc, char** argv){
 		radio.startListening();
 	}
 
-<<<<<<< HEAD
-	for(int i=0; i < 32; i++){
-=======
 	for(int i=0; i<32; i++){
->>>>>>> 47003bb3f46847facbc539dabe0048867f9784da
 		data[i] = rand() % 255;    //Load the buffer with random data
 	}
 	FILE *f = fopen("/home/pi/Desktop/data.csv","w");
 	//FILE *gnuplotFile = fopen("/home/pi/Desktop/data.dat","w");
-<<<<<<< HEAD
-	if(f == NULL){
-=======
+
 	if(f==NULL){
->>>>>>> 47003bb3f46847facbc539dabe0048867f9784da
 		printf("Error opening file!\n");
 		exit(1);
 	}
 	fprintf(f,"ArTime,Pi_Time,AcX,AcY,AcZ,GyX,GyY,GyZ\n"); //heading
 	// forever loop
-<<<<<<< HEAD
-	t0 = millis();
-	gnuplotTimer = millis();
-=======
 	t0=millis();
 	gnuplotTimer=millis();
->>>>>>> 47003bb3f46847facbc539dabe0048867f9784da
 	while (1){
 		if (role == role_ping_out){
 			sleep(2);
@@ -160,27 +124,6 @@ int main(int argc, char** argv){
 					counter = 0;
 					fclose(f);
 					FILE *f = fopen("/home/pi/Desktop/data.csv","a");
-<<<<<<< HEAD
-=======
-					
-					//pyStruct py = {(int16_t)(recievedata[1] << 8 | recievedata[2]),
-					//			 (int16_t)(recievedata[3] << 8 | recievedata[4]),
-					//			 (int16_t)(recievedata[5] << 8 | recievedata[6])};
-					//fwrite(&py, sizeof py, 1, stdout);
-
-					//data so gnuplot can graph it
-					//fprintf(gnuplotFile,"%d ",millis()-t0); //Time on  RPI 					
-					//acc data
-					//fprintf(gnuplotFile,"%d ", (int16_t)(recievedata[1] << 8 | recievedata[2])); //AcX
-					//fprintf(gnuplotFile,"%d ", (int16_t)(recievedata[3] << 8 | recievedata[4])); //AcY
-					//fprintf(gnuplotFile,"%d\n", (int16_t)(recievedata[5] << 8 | recievedata[6])); //AcZ
-
-					//if(millis() - gnuplotTimer > 1000){
-					//	gnuplotTimer = millis();
-					//	fclose(gnuplotFile);
-					//	FILE *gnuplotFile = fopen("/home/pi/Desktop/pydata.csv","w");					
-					//}	
->>>>>>> 47003bb3f46847facbc539dabe0048867f9784da
 				}
 			} 
 		  }
